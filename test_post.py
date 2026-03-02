4dd25e6dc599947e562127d8e2a91610755e5c11
Lucas Chung <lucashlc82@gmail.com>
2026-03-01 23:29:16 -0500

    Work March 1

--- test_post.py
+++ test_post.py
@@ -2,8 +2,29 @@ import os
 from dotenv import load_dotenv
 import tweepy
 
-#load environment variables
-load_dotenv
+# load environment variables
+load_dotenv()
 
 #Read keys from .env
+api_key = os.getenv("X_API_KEY")
+api_secret = os.getenv("X_API_SECRET")
+access_token = os.getenv("X_ACCESS_TOKEN")
+access_secret = os.getenv("X_ACCESS_SECRET")
 
+# Authenticate
+client = tweepy.Client(
+    consumer_key=api_key,
+    consumer_secret=api_secret,
+    access_token=access_token,
+    access_token_secret=access_secret
+)
+
+# Test tweet
+try:
+    response = client.create_tweet(
+        text="Hello! This is a test post from my bot"
+    )
+    print("Tweet posted successfully!")
+    print(response) 
+except Exception as e:
+    print("Error posting tweet:", e)
